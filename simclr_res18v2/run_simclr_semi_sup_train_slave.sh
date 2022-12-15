388f131a0be1ac996cd791eeae6521bf0c07bc14
Zhicheng Stark <stark.cheng@outlook.com>
2022-12-15 01:20:08 -0500

    Add files via upload

---
+++ simclr_res18v2/run_simclr_semi_sup_train_slave.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=run_simclr_base
+#SBATCH --time=3-00:00:00
+#SBATCH --mem=128GB
+#SBATCH --gres=gpu:2080rtx:1
+
+source /home/users/zg78/_conda/bin/activate
+conda activate base_torch
+
+nvidia-smi
+
+echo $1
+echo $2
+
+python simclr_semi_sup_train.py \
+    --pretrained_path $1 \
+    --label_percentage $2
+
